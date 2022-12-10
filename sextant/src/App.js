3419404ab6e3de0e05f20b6b10d2ade67ee62de4
Josef <josef.abdulkarim@hotmail.co.uk>
2022-12-10 16:52:25 +0000

    Added components

--- sextant/src/App.js
+++ sextant/src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
+function Banner(props){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App-banner'>
+      <p>Welcome to, {props.name}</p>
     </div>
-  );
+    )
+}
+
+function Card(props){
+  return(
+    <div className='Card'>
+      <header>{props.name}</header>
+      <p>network information</p>
+    </div>
+  )
+}
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Banner name="Sextant"></Banner>
+        <div className='Body'>
+          <Card name="Throughput"></Card>
+          <Card name="Ping"></Card>
+          <Card name="Devices"></Card>
+          <Card name="Devices"></Card>
+          <Card name="Devices"></Card>
+          <Card name="Devices"></Card>
+          <Card name="Devices"></Card>
+          <Card name="Devices"></Card>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
