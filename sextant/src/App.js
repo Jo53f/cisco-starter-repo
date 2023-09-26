c1d4b90851d10dcec7bacc440ee65bc3e835e18a
Josef <josef.abdulkarim@hotmail.co.uk>
2023-09-26 08:33:44 +0100

    improved readability

--- sextant/src/App.js
+++ sextant/src/App.js
@@ -11,15 +11,19 @@ class App extends Component {
       <div className="App">
         <Banner name="Sextant"></Banner>
         <div className='Body'>
+
           <Card name='IPv4 Address'>
             <IP url='https://api.ipify.org?format=json'/>
           </Card>
+
           <Card name='IPv6 Address'>
             <IP url ='https://api64.ipify.org?format=json'/>
           </Card>
+
           <Card name='Latency'>
             <Latency/>
           </Card>
+          
         </div>
       </div>
     )
